acba45f027fb2e461194b818728f1bbee08e0b28
Keaton Bell <keatonb@astro.as.utexas.edu>
2015-04-17 22:46:12 -0500

    example of how to compute confidence intervals with smoothing

---
+++ runsmoothed.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 
+
+#Generate list of boxcar smoothing functions and call ran_trials_2 with them.
+
+import numpy as np
+from astropy.convolution import convolve, Box1DKernel
+from ran_trials_2 import ran_trials
+
+#create list of functions
+metrics = []
+
+#define range of boxcar widths
+widths=[510] #1 muHz
+'''
+for w in widths:
+    metrics.append(lambda x,w=w: convolve(x, Box1DKernel(w)))
+    metrics.append(lambda x,w=w: convolve(x, Box1DKernel(w)))
+'''
+metrics.append(lambda x,w=510: convolve(x, Box1DKernel(w)))
+metrics.append(lambda x,w=510: convolve(x**2., Box1DKernel(w)))
+
+#run the trials
+ran_trials('exampledata.dat',nperm = 20,hifac=1.0,metrics=metrics)
+
+#plot the results:
+
